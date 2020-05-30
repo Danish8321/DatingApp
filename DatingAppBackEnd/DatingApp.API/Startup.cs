a220d9fe9058fbd782f910bf9a667889869264ce
Danish Ahmad <danish.8321@gmail.com>
2020-05-30 18:12:45 +0530

    Register Controller added

--- DatingAppBackEnd/DatingApp.API/Startup.cs
+++ DatingAppBackEnd/DatingApp.API/Startup.cs
@@ -19,10 +19,9 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-
             services.AddControllers();
-
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)

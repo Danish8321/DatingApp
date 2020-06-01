d87dfef1d6442fa71c3a5a3611759944a13dd32f
Danish Ahmad <danish.8321@gmail.com>
2020-06-01 21:30:13 +0530

    Register completed

--- DatingAPP-SPA/src/app/app.module.ts
+++ DatingAPP-SPA/src/app/app.module.ts
@@ -4,13 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent, NavComponent, HomeComponent, RegisterComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- DatingAPP-SPA/src/app/register/register.component.ts
+++ DatingAPP-SPA/src/app/register/register.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../services/auth.service';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  register(): void {
+    console.log(this.model);
+    this.authService.register(this.model).subscribe(
+      () => {
+        console.log('Register successful');
+
+      },
+      (error) => {
+        console.log(error);
+
+      }
+    );
   }
 
+  cancel(): void {
+    this.cancelRegister.emit(false);
+    console.log('cancel');
+  }
 }
